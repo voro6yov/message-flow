e04d699ecf9f3866b7f71f84d86bb4335a2069af
Valentin Vorobyev <51714257+voro6yov@users.noreply.github.com>
2024-03-22 11:19:26 +0500

    Fix headers serde (#20)

--- tests/message/test_message.py
+++ tests/message/test_message.py
@@ -35,6 +35,38 @@ def test_message__payload_and_headers():
     assert {"h1": {"h2": "h2 value"}, "message_id": "message_id_value"} == test_message.headers
 
 
+def test_message__deserialization():
+    class CorrelationIdTrait(MessageTrait):
+        message_id: str = Header()
+        correlation_id = CorrelationId("message_id")
+
+    class P2(BaseModel):
+        p2: str
+
+    class H1(BaseModel):
+        h2: str
+
+    class Example(Message):
+        message_info = MessageInfo(
+            title="Test Message",
+            summary="Test Message Summary",
+            description="Test Message Description",
+            traits=[CorrelationIdTrait],
+        )
+        p1: str = Payload()
+        p2: P2 = Payload()
+        h1: H1 = Header(H1(h2="h2 value"))
+
+    test_payload = b'{"p1":"p1 value","p2":{"p2":"p2 value"}}'
+    test_headers = {"h1": {"h2": "h2 value"}, "message_id": "message_id_value"}
+
+    test_message: Example = Example.from_payload_and_headers(test_payload, test_headers)
+
+    assert isinstance(test_message.p1, str)
+    assert isinstance(test_message.p2, P2)
+    assert isinstance(test_message.h1, H1)
+
+
 def test_message__payload_and_headers__with_default_factory():
     class Example(Message):
         p1: str = Payload(default_factory=lambda: uuid4().hex)
