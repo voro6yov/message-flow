50f51d898e7c4dcf912e71e7f40109019ccd05df
Valentin Vorobyev <voro6yov.valentin@gmail.com>
2024-04-07 01:13:40 +0500

    Add doc string to the Base Middleware

--- src/message_flow/app/base_middleware.py
+++ src/message_flow/app/base_middleware.py
@@ -1,22 +1,65 @@
 from contextlib import contextmanager
-from typing import Generator
+from typing import Annotated, Generator
+
+from typing_extensions import Doc
 
 from ..utils import external
 
 
 @external
 class BaseMiddleware:
-    def __init__(self, payload: bytes, headers: dict[str, str]) -> None:
+    """
+    `BaseMiddleware` class, used to define custom middlewares.
+
+    **Example**
+
+    ```python
+    import logging
+    from message_flow import BaseMiddleware
+
+    logger = logging.getLogger(__name__)
+
+
+    class CustomMiddleware(BaseMiddleware):
+        def on_consume(self) -> None:
+            logger.info("Message with %s payload and %s headers received.", self.payload, self.headers)
+            return super().on_consume()
+
+        def after_consume(self, error: Exception | None = None) -> None:
+            logger.info("Message with %s payload and %s headers processed.", self.payload, self.headers)
+            return super().after_consume(error)
+
+        def on_produce(self) -> None:
+            logger.info("Producing message with %s payload and %s headers.", self.payload, self.headers)
+            return super().on_produce()
+
+        def after_produce(self, error: Exception | None = None) -> None:
+            logger.info("Message with %s payload and %s headers produced.", self.payload, self.headers)
+            return super().after_produce(error)
+    ```
+    """
+
+    def __init__(
+        self,
+        payload: Annotated[bytes, Doc("Payload of the `Message`.")],
+        headers: Annotated[dict[str, str], Doc("Headers of the `Message`.")],
+    ) -> None:
         self.payload = payload
         self.headers = headers
 
     def on_consume(self) -> None:
+        """
+        Logic to execute before message processing.
+        """
         pass
 
     def after_consume(
         self,
         error: Exception | None = None,
     ) -> None:
+        """
+        Logic to execute after message processing.
+        """
         if error is not None:
             raise error
 
@@ -33,12 +76,18 @@ class BaseMiddleware:
         self.after_consume(consume_error)
 
     def on_produce(self) -> None:
+        """
+        Logic to execute before message producing.
+        """
         pass
 
     def after_produce(
         self,
         error: Exception | None = None,
     ) -> None:
+        """
+        Logic to execute after message producing.
+        """
         if error is not None:
             raise error
 
